4f44f05bea073a6112e4983dc8366a9ef579d7ee
krishna4157 <krishna.santho08@gmail.com>
2020-03-04 02:02:14 +0530

    added multiple locations

--- components/Maps.js
+++ components/Maps.js
@@ -8,6 +8,7 @@ import {
     Text,
     StatusBar,
     Animated,
+    Dimensions,
     PermissionsAndroid,
   } from 'react-native';
   
@@ -30,17 +31,45 @@ import { SpringScrollView } from "@youngtailors/react-native-spring-scrollview";
 import MapView,{PROVIDER_GOOGLE} from 'react-native-maps';
 import { Marker } from "react-native-maps";
 import { NavigationEvents } from 'react-navigation';
-
+import MapViewDirections from 'react-native-maps-directions'
 var arr = [faLanguage,faCog, faUser, faFax,faCoffee];
 var MoreItems = ['Language','Settings','Logout','Themes','Support Us'];
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.771707;
+const LONGITUDE = -122.4053769;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 class Maps extends Component {
     
   constructor(props){
     super(props);
-    this.state= {
-      paddingTop:1,
-    mapMargin:1
+    this.state = {
+paddingTop:1,
+      mapMargin:1,
+      coordinates: [
+        {
+          latitude: 37.3317876,
+          longitude: -122.0054812,
+        },
+        {
+          latitude: 37.771707,
+          longitude: -122.4053769,
+        },
+      ],
+    };
+ 
+    this.mapView = null;
     }
+
+
+    onMapPress = (e) => {
+      this.setState({
+        coordinates: [
+          ...this.state.coordinates,
+          e.nativeEvent.coordinate,
+        ],
+      });
     }
 
 
@@ -62,6 +91,9 @@ onMapRender=()=> {
 
 
     render() {
+      const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyC5dUTX1m25NeILTFp5BTS4mpy8tDylbnc';
       const{navigation}= this.props;
     //   const { subjectCompliance, retrieveSubjectCompliance, screenProps: { t } } = this.props;
         return (
@@ -73,12 +105,19 @@ onMapRender=()=> {
               console.log("hello")},3000);
             }} />
              <MapView 
+             initialRegion={{
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            }}
              showsPointsOfInterest
              showsScale
             userLocationAnnotationTitle
             zoomControlEnabled
             followsUserLocation
-            showsTraffic
+            onPress={this.onMapPress}
+            // showsTraffic
              ref={map => (this.map = map)}
              style={{marginBottom: this.state.mapMargin,flex:1}}
              provider='google'
@@ -88,6 +127,39 @@ onMapRender=()=> {
               this.onMapRender
              }
                     >
+                      {this.state.coordinates.map((coordinate, index) =>
+          <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
+        )}
+        {(this.state.coordinates.length >= 2) && (
+          <MapViewDirections
+            origin={this.state.coordinates[0]}
+            waypoints={ (this.state.coordinates.length > 2) ? this.state.coordinates.slice(1, -1): null}
+            destination={this.state.coordinates[this.state.coordinates.length-1]}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="hotpink"
+            optimizeWaypoints={true}
+            onStart={(params) => {
+              console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
+            }}
+            onReady={result => {
+              console.log('Distance: ${result.distance} km')
+              console.log('Duration: ${result.duration} min.')
+              
+              // this.mapView.fitToCoordinates(result.coordinates, {
+              //   edgePadding: {
+              //     right: (100 / 20),
+              //     bottom: (100 / 20),
+              //     left: (100 / 20),
+              //     top: (100 / 20),
+              //   }
+              // });
+            }}
+            onError={(errorMessage) => {
+              // console.log('GOT AN ERROR');
+            }}
+          />
+        )}
                      
             
                       </MapView>
